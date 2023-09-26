a93d25bc5af4116ac00a6fc819e988d793afe507
Marcus Ilgner <mail@marcusilgner.com>
2023-09-27 00:36:00 +0200

    refactor: introduce strategy pattern for retries

--- src/main/kotlin/com/cultureamp/eventsourcing/CommandGateway.kt
+++ src/main/kotlin/com/cultureamp/eventsourcing/CommandGateway.kt
@@ -3,23 +3,55 @@ package com.cultureamp.eventsourcing
 import arrow.core.Either
 import arrow.core.left
 
-interface CommandGateway<M : EventMetadata> {
-    companion object {
-        operator fun <M : EventMetadata> invoke(eventStore: EventStore<M>, vararg routes: Route<*, *, M>) = EventStoreCommandGateway(eventStore, *routes)
-    }
-    fun dispatch(command: Command, metadata: M, retries: Int = 5): Either<CommandError, SuccessStatus>
+interface RetryStrategy {
+    fun tryRunning(fn: () -> Either<CommandError, SuccessStatus>): Either<CommandError, SuccessStatus>
 }
 
-class EventStoreCommandGateway<M : EventMetadata>(private val eventStore: EventStore<M>, private vararg val routes: Route<*, *, M>) : CommandGateway<M> {
-    override tailrec fun dispatch(command: Command, metadata: M, retries: Int): Either<CommandError, SuccessStatus> {
-        val result = createOrUpdate(command, metadata)
-        return if (result is Either.Left && result.value is RetriableError && retries > 0) {
-            Thread.sleep(500L)
-            dispatch(command, metadata, retries - 1)
-        } else {
-            result
+// this mimics the default retry behaviour from earlier versions of Kestrel
+class FixedTriesThreadSleep(
+    private val retries: Int = 5,
+    private val delayMs: Long = 500L
+) : RetryStrategy {
+    override fun tryRunning(fn: () -> Either<CommandError, SuccessStatus>): Either<CommandError, SuccessStatus> {
+        for (i in retries downTo 0) {
+            val result = fn()
+            if (result.isLeft { it is RetriableError } && i > 0) {
+                Thread.sleep(delayMs)
+            } else {
+                return result
+            }
         }
+        throw RuntimeException("Managed to reach unreachable code")
     }
+}
+
+
+interface CommandGateway<M : EventMetadata> {
+    companion object {
+        operator fun <M : EventMetadata> invoke(
+            eventStore: EventStore<M>,
+            vararg routes: Route<*, *, M>
+        ) = EventStoreCommandGateway(eventStore, *routes)
+    }
+
+    fun dispatch(
+        command: Command,
+        metadata: M,
+        retryStrategy: RetryStrategy = FixedTriesThreadSleep()
+    ): Either<CommandError, SuccessStatus>
+}
+
+
+class EventStoreCommandGateway<M : EventMetadata>(
+    private val eventStore: EventStore<M>,
+    private vararg val routes: Route<*, *, M>
+) : CommandGateway<M> {
+    override fun dispatch(
+        command: Command,
+        metadata: M,
+        retryStrategy: RetryStrategy
+    ): Either<CommandError, SuccessStatus> =
+        retryStrategy.tryRunning { createOrUpdate(command, metadata) }
 
     private fun createOrUpdate(command: Command, metadata: M): Either<CommandError, SuccessStatus> {
         val constructor = constructorFor(command) ?: return NoConstructorForCommand.left()
@@ -28,14 +60,18 @@ class EventStoreCommandGateway<M : EventMetadata>(private val eventStore: EventS
             is CreationCommand -> constructor.create(command, metadata, eventStore).map { Created }
             else -> AggregateNotFound.left()
         } else when (command) {
-            is UpdateCommand -> constructor.update(command, metadata, events, eventStore).map { Updated }
+            is UpdateCommand -> constructor.update(command, metadata, events, eventStore)
+                .map { Updated }
+
             else -> AggregateAlreadyExists.left()
         }
     }
 
     @Suppress("UNCHECKED_CAST")
     private fun constructorFor(command: Command): AggregateConstructor<CreationCommand, CreationEvent, DomainError, UpdateCommand, UpdateEvent, M, Aggregate<UpdateCommand, UpdateEvent, DomainError, M, *>>? {
-        val route = routes.find { it.creationCommandClass.isInstance(command) || it.updateCommandClass.isInstance(command) }
+        val route = routes.find {
+            it.creationCommandClass.isInstance(command) || it.updateCommandClass.isInstance(command)
+        }
         return route?.aggregateConstructor as AggregateConstructor<CreationCommand, CreationEvent, DomainError, UpdateCommand, UpdateEvent, M, Aggregate<UpdateCommand, UpdateEvent, DomainError, M, *>>?
     }
 }
